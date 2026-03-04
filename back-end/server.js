f67efa5411ba5e3b9839bcb1b6195e8f405b48ec
Doriane <doriane_87@hotmail.fr>
2026-03-04 16:10:26 +0100

    test

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import "./config/env.js";
 import initDb from "./db/init.js";
-console.log("🚀 Démarrage du serveur…");
+
 const PORT = process.env.PORT;
 
 const startServer = async () => {
