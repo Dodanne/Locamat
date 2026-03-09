ab89c256b127dcb5e2264ce0b803132882adab41
Doriane <doriane_87@hotmail.fr>
2026-03-09 14:24:15 +0100

    fix bug nodemailler

--- back-end/services/email.service.js
+++ back-end/services/email.service.js
@@ -1,6 +1,8 @@
 import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
